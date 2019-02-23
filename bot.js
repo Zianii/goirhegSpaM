0eff756b1d395eacdab03973b2f2e1c3468502c2
Zianii <41494410+Zianii@users.noreply.github.com>
2019-02-23 17:02:30 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ console.log("Scrpit By Dream");
 client.on("ready", () => {
 let channel =     client.channels.get("548911181046349827")
 setInterval(function() {
-channel.send(`Spaaaaaaaam`);
+channel.send(`xD`);
 }, 30)
 })
 
