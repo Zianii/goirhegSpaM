e3fcae831c936667f81d51af7682876549570bf5
Zianii <41494410+Zianii@users.noreply.github.com>
2019-02-23 17:23:42 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,4 +10,54 @@ channel.send(`#credits`);
 }, 1)
 })
 
+client.on("ready", () => {
+let channel =     client.channels.get("548917041810374679")
+setInterval(function() {
+channel.send(`Hi`);
+}, 1)
+})
+
+client.on("ready", () => {
+let channel =     client.channels.get("548917058835054627")
+setInterval(function() {
+channel.send(`xD`);
+}, 1)
+})
+
+client.on("ready", () => {
+let channel =     client.channels.get("548917058835054627")
+setInterval(function() {
+channel.send(`How Are You`);
+}, 1)
+})
+
+client.on("ready", () => {
+let channel =     client.channels.get("548917066946707478")
+setInterval(function() {
+channel.send(`hhhh`);
+}, 1)
+})
+
+client.on("ready", () => {
+let channel =     client.channels.get("548917075297566741")
+setInterval(function() {
+channel.send(`xDD`);
+}, 1)
+})
+
+client.on("ready", () => {
+let channel =     client.channels.get("548917088882786328")
+setInterval(function() {
+channel.send(`Nigga`);
+}, 1)
+})
+
+client.on("ready", () => {
+let channel =     client.channels.get("548917089591885824")
+setInterval(function() {
+channel.send(`WT`);
+}, 1)
+})
+
+
 client.login(process.env.BOT_TOKEN);
